16a9c50a16a593499ddc0f45649231d53d88d7cb
Faded42k <jameshapurona@gmail.com>
2026-04-01 23:49:05 +1000

    Update generate.ts

--- api/generate.ts
+++ api/generate.ts
@@ -1,1 +1,8 @@
+export default async function handler(req, res) {
+  const chapter = `
+  The shadows moved before he did.
+  Something inside him had awakened...
+  `;
 
+  res.status(200).json({ chapter });
+}
